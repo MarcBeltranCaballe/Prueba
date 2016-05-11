48c843be0e60946956faa1d9d4024378529daae6
MarcBeltranCaballe <heldenhammer13@gmail.com>
2016-05-11 09:46:53 +0200

    Update ControlVersiones.java

--- ControlVersiones/src/controlversiones/ControlVersiones.java
+++ ControlVersiones/src/controlversiones/ControlVersiones.java
@@ -17,7 +17,24 @@ public class ControlVersiones {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //CAMBIOS HECHOS AQUI
+        for (int i = 0; i<10, i++) {
+            
+            switch (i) {
+                
+                case 1: System.out.println("1");
+                    break;
+                case 2: System.out.println("2");
+                    break;
+                case 3: System.out.println("3");
+                    break;
+                case 4: System.out.println("4");
+                    break;
+                case 5: System.out.println("5");
+                    break;
+                
+            }
+            
+        }
         
     }
     
